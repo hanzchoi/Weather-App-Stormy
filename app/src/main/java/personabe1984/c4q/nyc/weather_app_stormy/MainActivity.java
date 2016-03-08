6bee1d66175247cae2e7a055dbdaa9d456b56146
personabe1984 <personabe1984@gmail.com>
2016-03-07 21:18:46 -0500

    Added async task to process the call backs using OkHttp

--- app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/MainActivity.java
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -39,13 +40,26 @@ public class MainActivity extends AppCompatActivity {
 
         // All that is left is to execute the call
         // there is a method from the call class and returns a response object
-        try {
-            Response response = call.execute();
-            if(response.isSuccessful()){
-                Log.v(TAG , response.body().string());
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG,"Exception caught: ", e);
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if(response.isSuccessful()){
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG,"Exception caught: ", e);
+                }
+            }
+        });
+
+        Log.d(TAG,"Main UI is running!");
+
     }
 }
