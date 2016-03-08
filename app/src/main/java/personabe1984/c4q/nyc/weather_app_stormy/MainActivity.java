0e02841d8fe92c2d7bba1e3df7838c796c27300e
personabe1984 <personabe1984@gmail.com>
2016-03-07 20:35:36 -0500

    First commit

---
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/MainActivity.java
@@ -0,0 +1,51 @@
+package personabe1984.c4q.nyc.weather_app_stormy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String apiKey = "b1d867ce8f77a9747acbcdc2a3054b92";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        //Creating the okhttpclient object
+        OkHttpClient client = new OkHttpClient();
+        // creating a Request object that is from square http
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        // We put the request from the inside a call object
+        // The request is wrapped up in a call object
+        Call call = client.newCall(request);
+
+        // All that is left is to execute the call
+        // there is a method from the call class and returns a response object
+        try {
+            Response response = call.execute();
+            if(response.isSuccessful()){
+                Log.v(TAG , response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG,"Exception caught: ", e);
+        }
+    }
+}
