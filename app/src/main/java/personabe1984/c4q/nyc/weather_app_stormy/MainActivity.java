54e3e377e8e0b73794f7f5a0fbd24dd07173e94a
personabe1984 <personabe1984@gmail.com>
2016-03-08 21:10:43 -0500

    Created Weather object to help parse through the weather API

--- app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/MainActivity.java
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/MainActivity.java
@@ -1,8 +1,12 @@
 package personabe1984.c4q.nyc.weather_app_stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -26,45 +32,60 @@ public class MainActivity extends AppCompatActivity {
 
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude;
+        if(isNetworkAvailable()) {
+            //Creating the okhttpclient object
+            OkHttpClient client = new OkHttpClient();
+            // creating a Request object that is from square http
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
 
-        //Creating the okhttpclient object
-        OkHttpClient client = new OkHttpClient();
-        // creating a Request object that is from square http
-        Request request = new Request.Builder()
-                .url(forecastUrl)
-                .build();
+            // We put the request from the inside a call object
+            // The request is wrapped up in a call object
+            Call call = client.newCall(request);
 
-        // We put the request from the inside a call object
-        // The request is wrapped up in a call object
-        Call call = client.newCall(request);
+            // All that is left is to execute the call
+            // there is a method from the call class and returns a response object
 
-        // All that is left is to execute the call
-        // there is a method from the call class and returns a response object
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    Log.v(TAG, response.body().string());
-                    if(response.isSuccessful()){
-
-                    }
-                    else{
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG,"Exception caught: ", e);
                 }
-            }
-        });
 
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        }
+        else{
+            Toast.makeText(this, R.string.network_unavailable_message,
+                    Toast.LENGTH_LONG).show();
+        }
         Log.d(TAG, "Main UI is running!");
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
     }
 
     private void alertUserAboutError() {
