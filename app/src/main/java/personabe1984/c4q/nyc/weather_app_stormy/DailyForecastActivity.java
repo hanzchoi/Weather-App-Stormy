657e4f2891ed6635c895cd04fea6f96b20a825fd
personabe1984 <personabe1984@gmail.com>
2016-03-12 14:49:11 -0500

    added listview layout and activity for daily forecast

---
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/DailyForecastActivity.java
@@ -0,0 +1,13 @@
+package personabe1984.c4q.nyc.weather_app_stormy;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class DailyForecastActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+    }
+}
