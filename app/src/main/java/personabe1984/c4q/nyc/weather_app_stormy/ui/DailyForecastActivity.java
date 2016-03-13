5633fb80300bb80889ed2d308226de8919633859
personabe1984 <personabe1984@gmail.com>
2016-03-12 20:06:04 -0500

    added parcel and used them for the first time

--- app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/ui/DailyForecastActivity.java
@@ -1,7 +1,11 @@
 package personabe1984.c4q.nyc.weather_app_stormy.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+
+import java.util.Arrays;
 
 import personabe1984.c4q.nyc.weather_app_stormy.R;
 import personabe1984.c4q.nyc.weather_app_stormy.adapters.DayAdapter;
@@ -24,6 +28,9 @@ public class DailyForecastActivity extends ListActivity {
                 daysOfTheWeek);
         setListAdapter(adapter);
         */
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
         DayAdapter adapter = new DayAdapter(this, mDays );
     }
--- app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/ui/MainActivity.java
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/ui/MainActivity.java
@@ -37,6 +37,7 @@ import personabe1984.c4q.nyc.weather_app_stormy.weather.Hour;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String DAILY_FORECAST = "DAILY_FORECAST";
 
     private Forecast mForecast;
 
@@ -271,6 +272,7 @@ public class MainActivity extends AppCompatActivity {
     @OnClick (R.id.dailyButton)
     public void startDailyActivity(View view){
         Intent intent = new Intent(this, DailyForecastActivity.class);
+        intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
         startActivity(intent);
     }
 }
