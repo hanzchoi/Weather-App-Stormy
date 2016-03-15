fd92884483471121880fabc25d2a0d3eff8b2450
personabe1984 <personabe1984@gmail.com>
2016-03-15 12:21:10 -0400

    started to use recyclerView for the hourly portion of the app

---
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,15 @@
+package personabe1984.c4q.nyc.weather_app_stormy.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import personabe1984.c4q.nyc.weather_app_stormy.R;
+
+public class HourlyForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+    }
+}
