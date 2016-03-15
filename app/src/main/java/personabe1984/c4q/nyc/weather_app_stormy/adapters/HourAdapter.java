5dde0e7e95f79f917b42ae761156c500af2dc694
personabe1984 <personabe1984@gmail.com>
2016-03-15 19:08:34 -0400

    Added HourAdapter which uses Custom View holder for RecyclerView

--- app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/adapters/HourAdapter.java
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/adapters/HourAdapter.java
@@ -2,16 +2,45 @@ package personabe1984.c4q.nyc.weather_app_stormy.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import personabe1984.c4q.nyc.weather_app_stormy.weather.Hour;
+import personabe1984.c4q.nyc.weather_app_stormy.R;
 
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
 
+    @Override
+    public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(HourViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class HourViewHolder extends RecyclerView.ViewHolder{
 
-         public HourViewHolder(View itemView) {
-            super(itemView);
-         }
+        public TextView mTimeLabel;
+        public TextView mSummaryLabel;
+        public TextView mTemperatureLabel;
+        public ImageView mIconImageView;
+
+          public HourViewHolder(View itemView) {
+             super(itemView);
+
+             mTimeLabel = (TextView) itemView.findViewById(R.id.timeLabel);
+             mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
+             mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
+             mIconImageView = (ImageView) itemView.findViewById(R.id.iconImageView);
+
+          }
     }
 
 }
