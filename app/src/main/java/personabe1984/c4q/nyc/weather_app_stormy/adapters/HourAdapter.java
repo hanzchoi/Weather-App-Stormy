e30685a059994564b8b85d7f997815e1a6da858a
personabe1984 <personabe1984@gmail.com>
2016-03-16 19:57:29 -0400

    did the same this for the hourly forecast but again used the RecyclerView to implement the toast message on the list

--- app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/adapters/HourAdapter.java
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/adapters/HourAdapter.java
@@ -1,11 +1,13 @@
 package personabe1984.c4q.nyc.weather_app_stormy.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import personabe1984.c4q.nyc.weather_app_stormy.R;
 import personabe1984.c4q.nyc.weather_app_stormy.weather.Hour;
@@ -13,8 +15,10 @@ import personabe1984.c4q.nyc.weather_app_stormy.weather.Hour;
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
 
     private Hour[] mHours;
+    private Context mContext;
 
-    public HourAdapter(Hour[] hours){
+    public HourAdapter(Context context,Hour[] hours){
+        mContext = context;
         mHours = hours;
     }
 
@@ -38,7 +42,7 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
         return mHours.length;
     }
 
-    public class HourViewHolder extends RecyclerView.ViewHolder{
+    public class HourViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public TextView mTimeLabel;
         public TextView mSummaryLabel;
@@ -53,6 +57,8 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
             mIconImageView = (ImageView) itemView.findViewById(R.id.iconImageView);
 
+            itemView.setOnClickListener(this);
+
         }
 
         public void bindHour(Hour hour){
@@ -64,6 +70,15 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
         }
 
 
+        @Override
+        public void onClick(View v) {
+            String time = mTimeLabel.getText().toString();
+            String temperature = mTemperatureLabel.getText().toString();
+            String summary = mSummaryLabel.getText().toString();
+            String message = String.format("At %s it will be %s and %s", time,temperature,summary);
+
+            Toast.makeText(mContext, message,Toast.LENGTH_LONG).show();
+        }
     }
 
 }
