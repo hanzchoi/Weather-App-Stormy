3fe63c705172b3ce3ac7f507d4eea2f6cd440ee5
personabe1984 <personabe1984@gmail.com>
2016-03-15 21:27:50 -0400

    added recyclerView to generate the hourly weather

--- app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/personabe1984/c4q/nyc/weather_app_stormy/ui/HourlyForecastActivity.java
@@ -4,23 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.Arrays;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import personabe1984.c4q.nyc.weather_app_stormy.R;
+import personabe1984.c4q.nyc.weather_app_stormy.adapters.HourAdapter;
 import personabe1984.c4q.nyc.weather_app_stormy.weather.Hour;
 
 public class HourlyForecastActivity extends AppCompatActivity {
 
     private Hour[] mHours;
 
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
     }
 }
